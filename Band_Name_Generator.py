1259eade943f67f8f3b51f48973c673ba657db1a
Kodebykush <kodewithkush@gmail.com>
2024-11-15 09:52:21 +0530

    Add files via upload

---
+++ Band_Name_Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator!")
+
+city = input("what's the name of the city you grew up in?\n")
+pet_name = input("what's your first pet name?\n")
+
+print(f"your band name could be: The {city} {pet_name}")
---
+++ Super_Hero_Name_Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Superhero Name Generator!\nLet's find out your superhero identity.")
+
+color = input("What's your favorite color?\n")
+animal = input("What's your favorite animal?\n")
+year = int(input("What are the last two digits of your birth year?\n"))
+
+print(f"Your superhero name is: {color} {animal}{year}")
